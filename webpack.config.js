903481cfc3447b4bfb6f3af23c5cb9e92777c5d4
bronburgundy <bron.cotter@icloud.com>
2018-06-18 21:57:01 +1200

    set up file system and basic pages like index.html, webpack.config.js and index.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+import path from 'path'
+
+export const wp = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map'
+}
